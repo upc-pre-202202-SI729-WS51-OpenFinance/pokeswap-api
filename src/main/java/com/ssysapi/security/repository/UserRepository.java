9e11e62088bb55c2529ee8c315401224699a1cbe
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-11-20 22:12:23 -0500

    ft(security): implement repositories

---
+++ src/main/java/com/ssysapi/security/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ssysapi.security.repository;
+
+import com.ssysapi.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+    boolean existsByUserName(String userName);
+    boolean existsByEmail(String email);
+}
